5a740d1e10bf4b0d92f658c5f89486ae8c60e8b6
AntGod6123 <108909892+AntGod6123@users.noreply.github.com>
2025-06-08 22:05:20 +0200

    Add Tailwind setup and docs

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    './index.html',
+    './App.jsx',
+    './components/**/*.{js,jsx}',
+    './pages/**/*.{js,jsx}',
+    './main.jsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
